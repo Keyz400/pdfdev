c3c46e38b8873cc0225f6462f4d6eb4f2f1435e5
Keyz400 <109141199+Keyz400@users.noreply.github.com>
2025-06-07 17:53:45 +0530

    Update commands.py

--- ILovePDF/plugins/dm/commands.py
+++ ILovePDF/plugins/dm/commands.py
@@ -8,7 +8,6 @@ from pdf import PDF
 from plugins import *
 from plugins.utils import *
 from configs.db import myID
-from configs.beta import BETA
 from configs.db import dataBASE
 from configs.config import dm, settings
 
@@ -45,47 +44,8 @@ async def _cancelI2P(bot, message):
         await message.reply_text(trans_txt, quote=True)
 
 
-# ❌ BETA USER (/beta) ❌
-@ILovePDF.on_message(filters.private & filters.command(["beta"]) & filters.incoming)
-async def _betaMode(bot, message):
-    try:
-        lang_code = await util.getLang(message.from_user.id)
-        CHUNK, _ = await util.translate(text="BETA", lang_code=lang_code)
-
-        if message.chat.id in dm.ADMINS:
-            logger.debug(f"Beta Users:\n\n{BETA}\n\n")
-
-        if not dataBASE.MONGODB_URI:
-            return await message.reply_text(CHUNK["cant"], quote=True)
-
-        if (not message.chat.id in dm.ADMINS) and settings.REFER_BETA:
-            refer_ids = await db.get_key(id=message.chat.id, key="refer")
-            if (not refer_ids) or not (len(refer_ids.split("|")) >= 5):
-                return await message.reply_text(
-                    CHUNK["refer"].format(
-                        "0" if not refer_ids else f"`{refer_ids.replace('|', '`, `')}`",
-                        f"https://t.me/{myID[0].username}?start=-r{message.from_user.id}",
-                    ),
-                    quote=True,
-                )
-
-        if message.chat.id in dm.ADMINS and len(message.text.split(" ")) == 2:
-            await db.set_key(
-                id=int(message.text.split(" ")[1]), key="beta", value="True"
-            )
-            BETA.append(int(message.text.split(" ")[1]))
-            return await message.reply_text("Now He is a beta user", quote=True)
-
-        if message.chat.id not in BETA:
-            await db.set_key(id=message.chat.id, key="beta", value="True")
-            BETA.append(message.chat.id)
-            return await message.reply_text(CHUNK["nowbeta"], quote=True)
-        else:
-            await db.dlt_key(id=message.chat.id, key="banned")
-            BETA.remove(message.chat.id)
-            return await message.reply_text(CHUNK["nownotbeta"], quote=True)
-    except Exception as Error:
-        logger.exception("🐞 %s : %s" % (file_name, Error))
+# Beta functionality removed as requested
+# The beta command and all related functionality has been disabled
 
 # If you have any questions or suggestions, please feel free to reach out.
 # Together, we can make this project even better, Happy coding!  XD
