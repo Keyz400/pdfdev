339298e28a0a710d5e480da29df2b70a89aef627
Keyz400 <109141199+Keyz400@users.noreply.github.com>
2025-06-07 17:55:03 +0530

    Update waste.py

--- ILovePDF/plugins/dm/waste.py
+++ ILovePDF/plugins/dm/waste.py
@@ -9,12 +9,25 @@ from plugins.utils import *
 from configs.config import dm
 
 
-# WASTE/DPAMMING MESSAGES 
-@ILovePDF.on_message(filters.private & filters.incoming & ~filters.user(dm.ADMINS))
+# WASTE/SPAMMING MESSAGES 
+@ILovePDF.on_message(filters.private & filters.incoming & ~filters.user(dm.ADMINS) & ~filters.sticker)
 async def _spam(bot, message):
     try:
         lang_code = await util.getLang(message.chat.id)
         await message.reply_chat_action(enums.ChatAction.TYPING)
+        tTXT, tBTN = await util.translate(text="noHelp", lang_code=lang_code)
+        await message.reply_text(tTXT, quote=True)
+    except Exception as e:
+        logger.exception("🐞 %s: %s" % (file_name, e), exc_info=True)
+
+
+# HANDLE STICKERS - Removed specific sticker ID and disabled sticker responses
+@ILovePDF.on_message(filters.private & filters.sticker & filters.incoming)
+async def handle_stickers(bot, message):
+    try:
+        # Sticker handling disabled as requested
+        # The specific sticker ID "CAACAgIAAxkBAAEOjNtoMJIwYLbXxQYOWU0EcDgKON4TkAACVQADr8ZRGmTn_PAl6RC_NgQ" has been removed
+        lang_code = await util.getLang(message.chat.id)
         tTXT, tBTN = await util.translate(text="noHelp", lang_code=lang_code)
         await message.reply_text(tTXT, quote=True)
     except Exception as e:
